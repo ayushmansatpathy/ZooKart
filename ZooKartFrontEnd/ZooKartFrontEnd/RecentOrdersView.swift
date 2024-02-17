a5df2dbb42735cd5291e5ce0fe12d582dd464f2c
Kevin Mathew <kevinmathew423@hotmail.com>
2024-02-17 16:47:16 -0500

    added profile, orders

---
+++ ZooKartFrontEnd/ZooKartFrontEnd/RecentOrdersView.swift
@@ -0,0 +1,18 @@
+//
+//  RecentOrdersView.swift
+//  ZooKartFrontEnd
+//
+//  Created by Kevin Mathew on 2/17/24.
+//
+
+import SwiftUI
+
+struct RecentOrdersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentOrdersView()
+}
