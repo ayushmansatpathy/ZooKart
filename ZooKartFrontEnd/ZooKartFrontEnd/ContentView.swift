ee2234644cb181fabed7003b9d8a6b7a968c8ef2
Aaditya Saini <sainiraaditya@gmail.com>
2024-02-17 11:25:48 -0500

    added frontend

---
+++ ZooKartFrontEnd/ZooKartFrontEnd/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ZooKartFrontEnd
+//
+//  Created by Aaditya Saini on 2/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZooKartFrontEnd/ZooKartFrontEnd/ZooKartFrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZooKartFrontEndApp.swift
+//  ZooKartFrontEnd
+//
+//  Created by Aaditya Saini on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ZooKartFrontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
